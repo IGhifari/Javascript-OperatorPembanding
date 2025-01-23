60afdd9a95b543bd55bacef76dba98c174fe4ae0
IGhifari <rinnitoshii07@gmail.com>
2025-01-23 19:32:54 +0700

    belajar kondisi di javascript

---
+++ ifElse.js
@@ -0,0 +1,24 @@
+// if(1 + 1 === 2){
+//     console.log("The condition is true");
+// }else{
+//     console.log("Something went wrong");
+// }
+
+// let angka = Math.random();
+// console.log(angka);
+// if(angka >= 0.5){
+//     console.log("Angka lebih besar dari 0.5");
+// }else{
+//     console.log("Angka lebih kecil dari 0.5");
+// }
+
+const hari = "Senin";
+if(hari === "Senin"){
+    console.log("Hari ini adalah hari Senin");
+}else if(hari === "Selasa"){
+    console.log("Hari ini adalah hari Selasa");
+}else if(hari === "Rabu"){
+    console.log("Hari ini adalah hari Rabu");
+}else{
+    console.log("Hari ini bukan hari Senin, Selasa, atau Rabu");
+}
