b4d4645cf05182b5fa580eca30f0e888ea51f207
IGhifari <rinnitoshii07@gmail.com>
2025-01-24 19:13:50 +0700

    belajar operator logic and,or,not dan switch case

--- ifElse.js
+++ ifElse.js
@@ -12,13 +12,44 @@
 //     console.log("Angka lebih kecil dari 0.5");
 // }
 
-const hari = "Senin";
-if(hari === "Senin"){
-    console.log("Hari ini adalah hari Senin");
-}else if(hari === "Selasa"){
-    console.log("Hari ini adalah hari Selasa");
-}else if(hari === "Rabu"){
-    console.log("Hari ini adalah hari Rabu");
-}else{
-    console.log("Hari ini bukan hari Senin, Selasa, atau Rabu");
-}
+// const hari = "Senin";
+// if(hari === "Senin"){
+//     console.log("Hari ini adalah hari Senin");
+// }else if(hari === "Selasa"){
+//     console.log("Hari ini adalah hari Selasa");
+// }else if(hari === "Rabu"){
+//     console.log("Hari ini adalah hari Rabu");
+// }else{
+//     console.log("Hari ini bukan hari Senin, Selasa, atau Rabu");
+// }
+
+// const password  = document.getElementById("password").value;
+
+
+// if(password.length >= 6){
+//     if(password.indexOf(" ") === -1){
+//         console.log("Password valid");
+//     }else{
+//         console.log('password tidak boleh ada spasi');
+//     }
+    
+// }else{
+//     console.log("Password minimal 6 karakter");
+// }
+
+// let age = 19;
+// let gender = "male";
+
+// if(age => 19 && gender == "male"){
+//     console.log("Legal");
+// }
+
+// const checkPassword = () => {
+//     if(password.length >= 6 && password.indexOf(" ") === -1){
+//         alert("Password valid");
+//     }else{
+//         alert("Password tidak valid");
+//     }
+// }
+
+
---
+++ switchCase.js
@@ -0,0 +1,18 @@
+const day = "Senin";
+
+switch(day){
+    case "Senin":
+        console.log("Senin");
+        break;
+    case 2:
+        console.log("Selasa");
+        break;
+    case 3:
+        console.log("Rabu");
+        break;
+    case 4:
+        console.log("Kamis");
+        break;
+    default:
+        console.log("Hari tidak valid");
+}
